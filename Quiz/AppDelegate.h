52290fa7abf9bd1d6f3bf8682bb131a4bdef8aa5
Yasser Altamimi <yosy2010@gmail.com>
2018-09-15 01:59:23 +0300

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by YASSER ALTAMIMI on 15/09/2018.
+//  Copyright © 2018 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
